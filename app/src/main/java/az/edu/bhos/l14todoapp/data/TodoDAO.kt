58bd0de71c07c7a7ab165acafd5875e73d986154
Hackhoven <elmir.hacizade.eh@gmail.com>
2024-04-03 11:47:09 +0400

    commit

--- app/src/main/java/az/edu/bhos/l14todoapp/data/TodoDAO.kt
+++ app/src/main/java/az/edu/bhos/l14todoapp/data/TodoDAO.kt
@@ -1,8 +1,15 @@
 package az.edu.bhos.l14todoapp.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface TodoDao {
-    // TODO: fill the required methods
+    @Query("SELECT * FROM todos ORDER BY weekday ASC")
+    fun getAllTodos(): Flow<List<TodoLocalDto>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTodos(todos: List<TodoLocalDto>)
 }
--- app/src/main/java/az/edu/bhos/l14todoapp/data/TodoLocalData.kt
+++ app/src/main/java/az/edu/bhos/l14todoapp/data/TodoLocalData.kt
@@ -3,27 +3,25 @@ package az.edu.bhos.l14todoapp.data
 import az.edu.bhos.l14todoapp.data.dto.TodoLocalDto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 interface TodoLocalData {
     fun observeItems(): Flow<List<TodoLocalDto>>
     suspend fun save(data: List<TodoLocalDto>)
 }
 
+
 class TodoLocalDataImpl(
     private val todoDao: TodoDao
 ) : TodoLocalData {
 
     override fun observeItems(): Flow<List<TodoLocalDto>> {
-        // TODO: use todoDao
-        // TODO: return the items by `weekday` order (Monday, Tue, Wed..)
-        return flowOf(emptyList())
+        return todoDao.getAllTodos()
     }
 
     override suspend fun save(data: List<TodoLocalDto>) {
-        // TODO: use todoDao
-
-        // TODO: make sure your insert query will not cause conflict
-        //  (you will experience this on second insert attempt)
+        todoDao.insertTodos(data)
     }
-
 }
+
